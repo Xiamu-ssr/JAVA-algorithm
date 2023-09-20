860dd1915795b36ca84d3f60493c8c8b5647e820
Xiamu-ssr <1138882663@qq.com>
2023-09-20 17:58:44 +0800

    添加二分查找

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,2 +1,25 @@
+import java.util.List;
+
 public class BinarySearch {
+    private int[] arr = {1, 2, 4, 6, 7, 9};
+    private int target = 6;
+    public static void main(String[] args) {
+        BinarySearch bs = new BinarySearch();
+        int res = bs.Main();
+        System.out.println(res);
+    }
+    private int Main(){
+        int i=0, j=arr.length-1;
+        while (i<=j){
+            int m = (i+j) / 2;
+            if(target<arr[m]){
+                j=m-1;
+            }else if (target>arr[m]){
+                i=m+1;
+            }else {
+                return m;
+            }
+        }
+        return -1;
+    }
 }

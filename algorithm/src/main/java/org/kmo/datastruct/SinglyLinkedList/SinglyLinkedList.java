ede097db38c0790fd8d59077c92061cac41c172f
Xiamu-ssr <1138882663@qq.com>
2023-09-25 16:12:42 +0800

    添加二分查找

---
+++ algorithm/src/main/java/org/kmo/datastruct/SinglyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,134 @@
+package org.kmo.datastruct.SinglyLinkedList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+public class SinglyLinkedList implements Iterable<Integer>{
+    private Logger logger = LoggerFactory.getLogger("");
+    private Node head;
+    private Node tail;
+    private int size;
+    public SinglyLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+//    当内部类和外部内的成员变量无联系，可以加static（建议加）
+    private static class Node{
+        int value;
+        Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+//    增
+    public void addFirst(int value){
+        head = new Node(value, head);
+        size++;
+        if (size==1){
+            tail = head;
+        }
+    }
+    public void addLast(int value){
+        if (size==0){
+            addFirst(value);
+        }else {
+            tail.next = new Node(value,null);
+            tail = tail.next;
+            size++;
+        }
+    }
+    public void insert(int index, int value){
+        if (0<index && index<size){
+            Node p = findIndex(index-1);
+            p.next = new Node(value,p.next);
+            size++;
+        }else if (index==0){
+            addFirst(value);
+        }else if (index==size){
+            addLast(value);
+        }else {
+            throw illegalIndex(index);
+        }
+    }
+//    删
+    public void removeFirst(){
+        if (size==1){
+            head=null;
+            tail=null;
+            size=0;
+        }else if (size>1){
+            head = head.next;
+            size--;
+        }else {
+            logger.warn("size 0 none to remove");
+        }
+    }
+    public void remove(int index){
+        if (index<0 || size<=index){
+            throw illegalIndex(index);
+        }
+    }
+//    查
+    private Node findIndex(int index){
+        if (0<index && index<size-1){
+            int i=1;
+            for (Node p=head.next; p.next!=null; p=p.next,i++){
+                if (i==index){
+                    return p;
+                }
+            }
+        } else if (size!=0 && index == 0){
+            return head;
+        } else if (size!=0 && index==size-1){
+            return tail;
+        } else{
+            throw illegalIndex(index);
+        }
+        return null;
+    }
+    public int get(int index){
+        return findIndex(index).value;
+    }
+//    遍历
+    public void forEach2(Consumer<Integer> consumer){
+        Node p = head;
+        while (p!=null){
+            consumer.accept(p.value);
+            p = p.next;
+        }
+    }
+    @Override
+    public Iterator<Integer> iterator(){
+        return new Iterator<Integer>() {
+            private Node p = head;
+            @Override
+            public boolean hasNext() {
+                return p!=null;
+            }
+
+            @Override
+            public Integer next() {
+                Node tmp = p;
+                p = p.next;
+                return tmp.value;
+            }
+        };
+    }
+//    other
+    public int size(){
+        return size;
+    }
+    private IllegalArgumentException illegalIndex(int index){
+        return new IllegalArgumentException(String.format("index [%d] 不合法\n",index));
+    }
+}
+
+
---
+++ algorithm/src/test/java/org/kmo/datastruct/SinglyLinkedList/SinglyLinkedListTest.java
@@ -0,0 +1,36 @@
+package org.kmo.datastruct.SinglyLinkedList;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+public class SinglyLinkedListTest {
+    private Logger logger = LoggerFactory.getLogger("");
+    @Test
+    public void test(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.addLast(0);
+        sll.addFirst(1);
+        sll.addLast(2);
+        for (Integer e:sll){
+            System.out.printf("%d ",e);
+        }System.out.println();
+        logger.info("test01-add");
+        assertIterableEquals(List.of(1,0,2),sll);
+        logger.info("test02-get");
+        assertEquals(2,sll.get(2));
+        logger.info("test03-size");
+        assertEquals(3,sll.size());
+        sll.insert(0,-1);
+        sll.insert(1,100);
+        sll.insert(sll.size()-1,888);
+        sll.insert(sll.size(),999);
+        logger.info("test04-insert");
+        assertIterableEquals(List.of(-1,100,1,0,888,2,999),sll);
+    }
+}
